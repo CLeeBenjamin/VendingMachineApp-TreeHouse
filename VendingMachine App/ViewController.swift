3b1ef8ed421569af3b94db45799dfa5b960c1178
Caston Boyd <castonboyd@gmail.com>
2018-04-23 17:28:40 -0400

    Initial Commit

---
+++ VendingMachine App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VendingMachine App
+//
+//  Created by Caston  Boyd on 4/23/18.
+//  Copyright © 2018 Caston  Boyd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
