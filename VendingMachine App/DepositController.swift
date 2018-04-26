eed7400bbfc9a27ed4ee79ecfc9de1b92040b40f
Caston  Boyd <castonboyd@Castons-MacBook-Pro.local>
2018-04-26 17:08:55 -0400

    initial commit

---
+++ VendingMachine App/DepositController.swift
@@ -0,0 +1,30 @@
+//
+//  DepositController.swift
+//  VendingMachine App
+//
+//  Created by Caston  Boyd on 4/26/18.
+//  Copyright © 2018 Caston  Boyd. All rights reserved.
+//
+
+import UIKit
+
+class DepositController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func dismiss(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+}
---
+++ VendingMachine App/VendingItemCell.swift
@@ -0,0 +1,15 @@
+//
+//  VendingItemCell.swift
+//  VendingMachine App
+//
+//  Created by Caston  Boyd on 4/23/18.
+//  Copyright © 2018 Caston  Boyd. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class VendingItemCell: UICollectionViewCell {
+    
+    @IBOutlet weak var iconView: UIImageView!
+}
